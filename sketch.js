ff754519bf5f8ea146e7fd08b437a52d1dd442a1
Martijn Kunstman <martijn@kunstman.nl>
2024-10-11 11:21:42 +0200

    check answers

--- sketch.js
+++ sketch.js
@@ -1,27 +1,26 @@
-// let vraag1 = ["Hoeveel is 1 + 1?", "2", "3", 0];
-// let vraag2 = ["Hoeveel is 2 + 4?", "8", "6", 1];
-// let vraag3 = ["Welke kleur heeft een banaan?", "Geel", "Blauw", 0];
-// let vragen_allemaal = [vraag1, vraag2, vraag3];
-
 let vragen_allemaal = [
   ["Hoeveel is 1 + 1?", "2", "3", 0],
   ["Hoeveel is 2 + 4?", "8", "6", 1],
   ["Welke kleur heeft een banaan?", "Geel", "Blauw", 0],
 ];
 
+let button1;
+let button2;
+
 let score = 0;
-let vraag_nummer = 2;
+let vraag_nummer = 0;
+let welke_vraag_is_goed;
 
 function setup() {
   createCanvas(400, 400);
   background(220);
+  button1 = createButton("");
+  button2 = createButton("");
+  maakNieuweVraag();
+}
 
-  //maakVraag("Hoeveel is 1 + 1?", "2", "3", 0);
-
-  //maakVraag("Hoeveel is 2 + 4?", "8", "6", 1);
-
-  //maakVraag("Welke kleur heeft een banaan?", "Geel", "Blauw", 0);
-
+function maakNieuweVraag() {
+  background(220);
   maakVraag(
     vragen_allemaal[vraag_nummer][0],
     vragen_allemaal[vraag_nummer][1],
@@ -30,20 +29,55 @@ function setup() {
   );
 }
 
-function controlleerAntwoord() {
-  //CONTROLEREN OF HET GOED IS...
-  console.log("click");
+function controlleerAntwoord1() {
+  if (welke_vraag_is_goed == 0) {
+    button1.style("background-color", "green");
+    score++;
+  } else {
+    button1.style("background-color", "red");
+  }
+  vraag_nummer++;
+  if (vraag_nummer >= vragen_allemaal.length) {
+    setTimeout(theEnd, 2000);
+  } else {
+    setTimeout(maakNieuweVraag, 2000);
+  }
+}
+function controlleerAntwoord2() {
+  if (welke_vraag_is_goed == 1) {
+    button2.style("background-color", "green");
+    score++;
+  } else {
+    button2.style("background-color", "red");
+  }
+  vraag_nummer++;
+  if (vraag_nummer >= vragen_allemaal.length) {
+    setTimeout(theEnd, 2000);
+  } else {
+    setTimeout(maakNieuweVraag, 2000);
+  }
+}
+
+function theEnd() {
+  background(220);
+  button1.remove();
+  button2.remove();
+  text("Je score is: " + score, 10, 10);
 }
 
 function draw() {}
 
 function maakVraag(vraag, antwoord1, antwoord2, welkeIsGoed) {
   text(vraag, 10, 10);
-  let button1 = createButton(antwoord1);
+  button1.style("background-color", "white");
+  button1.html(antwoord1);
   button1.position(10, 30);
-  button1.mousePressed(controlleerAntwoord);
+  button1.mousePressed(controlleerAntwoord1);
 
-  let button2 = createButton(antwoord2);
+  button2.style("background-color", "white");
+  button2.html(antwoord2);
   button2.position(10, 50);
-  button2.mousePressed(controlleerAntwoord);
+  button2.mousePressed(controlleerAntwoord2);
+
+  welke_vraag_is_goed = welkeIsGoed;
 }
